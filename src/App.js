d54200c053ae7bc9ef641bfe1be7256bce8db89f
francis-nguyen92 <francishtknguyen@gmail.com>
2021-03-19 07:12:39 -0500

    Create a CodeGrade submission

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import {BASE_URL} from './Constants/index'
+import Character from './components/Character'
+import styled from 'styled-components'
+
+const StyleDiv = styled.div`
+    color : #FFE81F;
+    font-weight: bold;
+    background-color: black;
+    padding: 5%;
+    margin: 5%;
+    h1{
+      font-family: 'News Gothic';
+      -webkit-text-stroke: 2px #FFE81F;
+      font-size: 3rem;
+      border: solid 1px #FFE81F;
+      &:hover{
+        transform:scale(2);
+        transition: all 1s ease-out;
+      }
+    }
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [data, setData] = useState(null)
+  const [dataType, setDataType] = useState(['people'])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get(`${BASE_URL}${dataType}`)
+    .then(res => {
+      console.log(res.data);
+      setData(res.data);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  if(!data) return <h1>Loading....</h1>
 
   return (
-    <div className="App">
+    
+    <StyleDiv className="App">
       <h1 className="Header">Characters</h1>
-    </div>
+      {data.map(element => {
+        return <Character key ={element.height} data={element} />
+      })}
+    </StyleDiv>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,23 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components'
+
+const StyleDiv = styled.div`
+    color : #FFE81F;
+    border: solid 1px blue;
+    font-weight: bold;
+    background-color: black;
+    margin: 1rem;
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+`
+export default function Character({data}) {
+
+    return(
+        <StyleDiv>
+           <div>{data.name}</div>
+           <div>Birth Year: {data.birth_year}</div>
+           <div>Mass: {data.mass}</div>
+        </StyleDiv>
+    )
+}
